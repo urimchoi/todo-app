93ce4de43f27e35774f12eeab6cabf4d20891c76
Urim Choi <urim.choi@gmail.com>
2014-07-03 14:19:49 -0400

    finished practice

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 Todos = Ember.Application.create();
 
-Todos.ApplicationAdapter = DS.FixtureAdapter.extend();
+Todos.ApplicationAdapter = DS.LSAdapter.extend({
+	namespace: 'todos-emberjs'
+});
 
 
 
