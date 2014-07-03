2e2c830c4d3f26b3a688850bcdaaeba8c1de4107
Urim Choi <urim.choi@gmail.com>
2014-07-03 11:39:48 -0400

    07/02/14 Changes

--- js/app.js
+++ js/app.js
@@ -2,9 +2,5 @@ Todos = Ember.Application.create();
 
 Todos.ApplicationAdapter = DS.FixtureAdapter.extend();
 
-Todos.Router.map(function () {
-  this.resource('todos', { path: '/' });
-});
-
 
 
--- js/controllers/todo_controller.js
+++ js/controllers/todo_controller.js
@@ -1,4 +1,27 @@
 Todos.TodoController = Ember.ObjectController.extend({
+	actions: {
+		editTodo: function() {
+			this.set('isEditing', true);
+			console.log('editing!');
+		    console.log(this);
+		},
+		acceptChanges: function() {
+		    this.set('isEditing', false);
+		    
+		    if (Ember.isEmpty(this.get('model.title'))) {
+		      this.send('removeTodo');
+		    } else {
+		      this.get('model').save();		    }
+		},
+		removeTodo: function () {
+		  	var todo = this.get('model');
+		  	todo.deleteRecord();
+		  	todo.save();
+		}
+	},
+
+	isEditing: false,
+
 	isCompleted: function(key, value){
 		var model = this.get('model');
 
