53545485c6cb99145fa4371dc3fd0adcc478068e
Urim Choi <urim.choi@gmail.com>
2014-07-03 12:10:13 -0400

    update

--- js/controllers/todos_controller.js
+++ js/controllers/todos_controller.js
@@ -16,8 +16,25 @@ Todos.TodosController = Ember.ArrayController.extend({
 
       // Save the new model
       todo.save();
+    },
+    clearCompleted: function() {
+      var completed = this.filterBy('isCompleted', true);
+      completed.invoke('deleteRecord');
+      completed.invoke('save');
     }
   },
+
+  allAreDone: function(key, value) {
+    return !!this.get('length') && this.isEvery('isCompleted');
+  }.property('@each.isCompleted'),
+
+  hasCompleted: function() {
+    return this.get('completed') > 0;
+  }.property('completed'),
+
+  completed: function() {
+    return this.filterBy('isCompleted', true).get('length');
+  }.property('@each.isCompleted'),
 
   remaining: function() {
   	return this.filterBy('isCompleted', false).get('length');
